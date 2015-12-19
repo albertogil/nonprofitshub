6da090d01bdbdb3e87fa606bf8a75e9249558663
Alberto Gil <albertogil@Albertos-MacBook-Pro.local>
2015-12-19 14:13:55 -0600

    Adding footer menu

--- nonprofitshub-theme/templates/footer.php
+++ nonprofitshub-theme/templates/footer.php
@@ -1,5 +1,10 @@
 <footer class="content-info">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <?php
+        if (has_nav_menu('footer_navigation')) :
+            wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav']);
+        endif;
+    ?>
   </div>
 </footer>
